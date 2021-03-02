f217de73d4b39989d6702c9ee3ae48d4f177e842
chandini21 <chandinisri.805@gmail.com>
2021-03-02 06:51:33 -0600

    Create rightRotateList.py

---
+++ rightRotateList.py
@@ -0,0 +1,10 @@
+""" Implement a function right_rotate(lst, k) which will rotate the given list by k. This means that the right-most elements will appear at the left-most position in the list and so on. You only have to rotate the list by one element at a time. """
+
+def right_rotate(lst, k):
+    # Write your code here
+    for i in range(k):
+        lst = [lst[-1]] + lst [:-1]
+       # res = lst[:-1]
+        #lst = last + res 
+        #print(last, res)
+    return lst
